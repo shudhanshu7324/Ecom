c0b8046732360452aec2ad65fc798ab4b02ea13b
Shudhanshu Shree <shudhanshu7324@gmail.com>
2025-01-17 01:00:41 +0530

    authentication complete

---
+++ controllers/user.controller.js
@@ -0,0 +1,44 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ message: "User loggedin successfully! ", token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const dotenv = require('dotenv');
+const userRouter = require('./routes/user.route');
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+// routes
+app.use('/api/user',userRouter);
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
