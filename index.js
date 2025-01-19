f5f456bb14e4d7ede4b2d9a8355e1d58fa37b2a6
Shudhanshu Shree <shudhanshu7324@gmail.com>
2025-01-19 12:12:59 +0530

    cors add

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const userRouter = require('./routes/user.route');
 const productRouter = require('./routes/product.route');
 const isLoggedIn = require('./middlewares/auth.middleware');
 dotenv.config();
+const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 
 // middleware
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json({limit:'10mb'}));
+app.use(express.urlencoded({limit:'10mb',extended:true}));
+app.use(cors());
 
 // routes
 app.use('/api/user',userRouter);
